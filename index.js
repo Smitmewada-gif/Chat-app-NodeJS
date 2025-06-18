520cee4db584b5fe127c94f63525959c70a3619c
smit_mewada <smit.mewada@instinctinnovations.com>
2025-06-18 22:07:07 +0530

    Basic socket setup done

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const socketio= require("socket.io");
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('from_client',()=>{
+    console.log('event coming from client')
+  })
+
+  setInterval(()=>{
+    socket.emit("from_server");
+  }, 2000);
+});
+
+app.use('/', express.static(__dirname + '/public'));
+server.listen(3000, ()=>{
+  console.log('Server started');
+})
---
+++ public/script.js
@@ -0,0 +1,11 @@
+var socket = io();
+let btn = document.getElementById('btn');
+btn.onclick = function exec(){
+  socket.emit('from_client');
+}
+socket.on('from_server', ()=>{
+  console.log("collected a new evert from server");
+  const div = document.createElement('div');
+  div.innerText = 'New event from server';
+  document.body.appendChild(div);
+})
